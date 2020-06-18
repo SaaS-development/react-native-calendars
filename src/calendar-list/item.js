fb27e5881ecd36586745946d0c6d961996ebed2c
matthewadel <matthewadel69@yahoo.com>
2020-06-18 18:32:59 +0200

    change calenderHeight

--- src/calendar-list/item.js
+++ src/calendar-list/item.js
@@ -53,7 +53,8 @@ class CalendarListItem extends Component {
       return (
         <Calendar
           theme={this.props.theme}
-          style={[{ height: this.props.calendarHeight, width: this.props.calendarWidth }, this.style.calendar, this.props.style]}
+          // style={[{ height: this.props.calendarHeight, width: this.props.calendarWidth }, this.style.calendar, this.props.style]}
+          style={[{ width: this.props.calendarWidth }, this.style.calendar, this.props.style]}
           current={row}
           hideArrows={this.props.hideArrows}
           hideExtraDays={this.props.hideExtraDays}
